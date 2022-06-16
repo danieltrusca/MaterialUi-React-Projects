cac8f77fdc90985d5ba839ecc4ef5f2c73d8aba8
danieltrusca <truscadani@yahoo.com>
2022-06-16 12:38:40 +0300

    working on Horizontal Scrollbar

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -5,9 +5,8 @@ import { exerciseOptions, fetchData } from "../utils/fetchData";
 
 import HorizontalScrollbar from "./HorizontalScrollbar";
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState("");
-  const [exercises, setExercises] = useState([]);
   const [bodyParts, setBodyParts] = useState([]);
 
   useEffect(() => {
@@ -89,12 +88,18 @@ const SearchExercises = () => {
           Search
         </Button>
       </Box>
-      <Box sx={{
-        position: 'relative',
-        width: '100%',
-        p: '20px'
-      }}>
-          <HorizontalScrollbar data={bodyParts} />
+      <Box
+        sx={{
+          position: "relative",
+          width: "100%",
+          p: "20px",
+        }}
+      >
+        <HorizontalScrollbar
+          data={bodyParts}
+          setBodyPart={setBodyPart}
+          bodyPart={bodyPart}
+        />
       </Box>
     </Stack>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import { Box } from '@mui/material';
+import { Box } from "@mui/material";
 
 import HeroBanner from "../components/HeroBanner";
 import SearchExercises from "../components/SearchExercises";
 import Exercises from "../components/Exercises";
 
 const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState("all");
   return (
     <Box>
-        <HeroBanner />
-        <SearchExercises />
-        <Exercises />
+      <HeroBanner />
+      <SearchExercises
+        setExercises={setExercises}
+        setBodyPart={setBodyPart}
+        bodyPart={bodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        exercises={exercises}
+        bodyPart={bodyPart}
+      />
     </Box>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
